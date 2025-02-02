582ef18dddfb6406ece27f41bad80de9de84b29f
Mirco Kraenz <software@kraenz.eu>
2025-02-02 16:09:36 +0100

    fix(schemas): sprite schemas all-in-one + typos
    #3

--- packages/schemas/src/lib/spritedata/sprites.ts
+++ packages/schemas/src/lib/spritedata/sprites.ts
@@ -14,11 +14,18 @@ export type DmItemSprites = {
 };
 
 export type DmMonsterSprite = DmSpriteBase & {
+  /** Only needed for monsters, npcs, and other actors. Not needed for Item sprites.  */
   width: 80;
+  /** Only needed for monsters, npcs, and other actors. Not needed for Item sprites.  */
   height: 128;
 };
 
 /** A map from entityDefName to the values inside that entitydef. The name will appear in `entityDef "<NAME_HERE>". The value is used to generate the body of the entityDef. */
 export type DmMonsterSprites = {
   [entityDefName: string]: DmMonsterSprite;
+};
+
+/** A map from entityDefName to the values inside that entitydef. The name will appear in `entityDef "<NAME_HERE>". The value is used to generate the body of the entityDef. */
+export type DmSprites = {
+  [entityDefName: string]: DmMonsterSprite | DmItemSprite;
 };
