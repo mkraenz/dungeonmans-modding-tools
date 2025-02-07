1edd017f63005df5b1fa75721604d0947d1ea8e3
Mirco Kraenz <software@kraenz.eu>
2025-02-07 22:35:05 +0100

    fix(schemas): improve docs of monsters and NPCs

--- packages/schemas/src/lib/academydata/academy.ts
+++ packages/schemas/src/lib/academydata/academy.ts
@@ -1,12 +1,51 @@
 import { DoubleDigit } from '../ts-utils.js';
-
 type WithExtraActors = {
   [key in `extraactor_${DoubleDigit}`]?: string;
 };
 
+type SpawnLocationNextTo =
+  | 'headmaster'
+  | 'librarian'
+  | 'alchemist'
+  | 'curator'
+  | 'research'
+  | 'armorer'
+  | 'gravedigger'
+  | 'battlemaster'
+  | 'hatch'
+  | 'pigfarmer';
+
+/**
+ * A string tuple of format "reference_to_an_actor,spawn_location".
+ * reference_to_an_actor must be an entity in actordata/.
+ * spawn_location must be one of the following values: 'headmaster, librarian, alchemist, curator, research, armorer, gravedigger, battlemaster, hatch, pigfarmer'
+ * Example: "extractor": "mymod_npc,headmaster"
+ */
+export type ExtraActor = `${string},${SpawnLocationNextTo}`;
+
+/** You can use this to add new NPCs to the academy. */
 export type DmAcademyExtraData = {
   class: 'dmAcademyExtraData';
-} & WithExtraActors;
+  extraactor_01?: ExtraActor;
+  extraactor_02?: ExtraActor;
+  extraactor_03?: ExtraActor;
+  extraactor_04?: ExtraActor;
+  extraactor_05?: ExtraActor;
+  extraactor_06?: ExtraActor;
+  extraactor_07?: ExtraActor;
+  extraactor_08?: ExtraActor;
+  extraactor_09?: ExtraActor;
+  extraactor_10?: ExtraActor;
+  extraactor_11?: ExtraActor;
+  extraactor_13?: ExtraActor;
+  extraactor_14?: ExtraActor;
+  extraactor_15?: ExtraActor;
+  extraactor_16?: ExtraActor;
+  extraactor_17?: ExtraActor;
+  extraactor_18?: ExtraActor;
+  extraactor_19?: ExtraActor;
+  extraactor_20?: ExtraActor;
+};
 
 /** Dictionary from entitydef name to DmAcademyExtraData. */
 export type DmAcademy = {
--- packages/schemas/src/lib/actordata/actors.ts
+++ packages/schemas/src/lib/actordata/actors.ts
@@ -1,23 +1,39 @@
 import { DoubleDigit } from '../ts-utils.js';
 
+/**
+ * The base damage an attack does. A string triple of format "damage_type,dice_roll,???".
+ * - List of damage types: melee, ranged, fire, ice, dark, starlight, poison, bleed.
+ *   It might be that you can also combine multiple types using an & character, for example "melee&dark&poison,1d6+35,0" but I am not sure whether that works and what exactly it does.
+ * - dice_roll can be either a number or the TTRPG dice notation.
+ * - ??? I think this is always 0 for damage dealt. This third number only becomes relevant for `damagetaken01` properties where it seems to represent how much more damage is being taken by this type of attack, effectively meaning elemental weaknesses.
+ * @examples ["melee,1,0", "fire,2d8+30,0", "poison,1,0"]
+ */
+export type Damage = string;
+/** Reference to an entity of class "dmSpecialPower" in specialpowerdata/. */
+export type PowerRef = string;
+
 type WithMeleeDamage = {
-  [key in `meleedamage_${DoubleDigit}`]?: string;
+  [key in `meleedamage_${DoubleDigit}`]?: Damage;
 };
 type WithRangedDamage = {
-  [key in `rangeDamage_${DoubleDigit}`]?: string;
+  [key in `rangeDamage_${DoubleDigit}`]?: Damage;
 };
 type WithPower = {
-  [key in `power_${DoubleDigit}`]?: string;
+  [key in `power_${DoubleDigit}`]?: PowerRef;
 };
+/** Docs at https://dungeonmans.fandom.com/wiki/Mods:_Adding_Monsters */
 export type DmMonster = {
+  /** The type of class. Constant. */
   class: 'dmMonster';
+  /** Whether it is blocked to swap places with the target by bumping into the target. Typically true, i.e. monsters block your path and vice-versa. */
   bBlockTileEntry: true;
   /** reference to an entitydef inside spritedata/ */
   sprite: string;
+  /** Display name */
   name: string;
   /** @asType integer */
   health: number;
-  /** entitydefs of class dmCoins inside ActorData/coins.txt */
+  /** Reference to an entitydefs of class dmCoins inside ActorData/coins.txt */
   cointype:
     | (string & {})
     | 'copper_coins_small'
@@ -42,8 +58,10 @@ export type DmMonster = {
   scale_adjustdamage: number;
   // yep, max 9 meleedamage. We could get more complicated using `[key in `meleedamage_${digit}${digit}`]?: string` but i'm not sure whether it works with the json schema generation. Anyway, 9 types of meleedamage should probably suffice for a good while.
   nametable: string;
+  /** Whether the actor has ranged attacks. If true, set the several properties starting with ranged* and tooclosedistance. */
   hasRanged: boolean;
 
+  /** TODO: This probably is the projectile sprite.  */
   rangedAttackSprite?: string;
   /** @asType integer */
   rangedAttackRange?: number;
