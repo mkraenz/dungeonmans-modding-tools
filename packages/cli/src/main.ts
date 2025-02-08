5dfa1978c2a0ae556f4048f594d3136c6efb978b
Mirco Kraenz <software@kraenz.eu>
2025-02-08 21:21:44 +0100

    chore(cli): cli version from package.json

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from '@commander-js/extra-typings';
+import packagejson from '../package.json' assert { type: 'json' };
 import { createBuildCommand } from './build/build.command.js';
 import { createInitCommand } from './init/init.command.js';
 import { createSchemasCommand } from './schemas/schemas.command.js';
@@ -11,7 +12,7 @@ const program = new Command();
 program
   .name('@dungeonmans-mod-tools/cli')
   .description('CLI to help you develop mod content for Dungeonmans.')
-  .version('0.2.1');
+  .version(packagejson.version);
 program.addCommand(createInitCommand());
 program.addCommand(createBuildCommand());
 program.addCommand(createSchemasCommand());
