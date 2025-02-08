e5ed5bd2d0f09607bf2281a20b218d6808e5ca6c
Mirco Kraenz <software@kraenz.eu>
2025-02-08 21:04:29 +0100

    refactor(js-to-entitydef): use a class

--- packages/js-to-entitydef/src/lib/js-to-entitydef.ts
+++ packages/js-to-entitydef/src/lib/js-to-entitydef.ts
@@ -8,37 +8,56 @@ const defaultOptions = {
   stripPrefix: '',
 };
 
+class JsToEntityDefTransformer {
+  private readonly options: Required<Options>;
+
+  constructor(
+    private readonly entityName: string,
+    private readonly keyValuePairs: Record<string, EntityDefKeyValuePairValue>,
+    options: Options = {}
+  ) {
+    this.options = { ...defaultOptions, ...options };
+  }
+
+  toEntityDef() {
+    const header = `entityDef "${this.entityName}"`;
+    const rows = Object.entries(this.keyValuePairs).map(
+      ([key, value]) =>
+        `    ${this.toPrintedKey(
+          key,
+          this.options.stripPrefix
+        )} ${this.toPrintedValue(value, this.options.stripPrefix)}`
+    );
+
+    const lines = [header, '{', ...rows, '}'];
+    return lines.join('\n').concat('\n'); // add a final empty line
+  }
+
+  private toPrintedKey(key: string, stripPrefix: string) {
+    const strippedKey = key.startsWith(stripPrefix)
+      ? key.replace(stripPrefix, '')
+      : key;
+    if (key.includes(' ')) return `"${strippedKey}"`;
+    return strippedKey;
+  }
+
+  private toPrintedValue(val: EntityDefKeyValuePairValue, stripPrefix: string) {
+    if (typeof val === 'string')
+      return `"${
+        val.startsWith(stripPrefix) ? val.replace(stripPrefix, '') : val
+      }"`;
+    return val;
+  }
+}
+
 export function jsToEntitydef(
   name: string,
   keyValuePairs: Record<string, EntityDefKeyValuePairValue>,
   options: Options = {}
 ): string {
-  const opts = { ...defaultOptions, ...options };
-  const header = `entityDef "${name}"`;
-  const rows = Object.entries(keyValuePairs).map(
-    ([key, value]) =>
-      `    ${toPrintedKey(key, opts.stripPrefix)} ${toPrintedValue(
-        value,
-        opts.stripPrefix
-      )}`
-  );
-
-  const lines = [header, '{', ...rows, '}'];
-  return lines.join('\n').concat('\n'); // add a final empty line
-}
-
-function toPrintedKey(key: string, stripPrefix: string) {
-  const strippedKey = key.startsWith(stripPrefix)
-    ? key.replace(stripPrefix, '')
-    : key;
-  if (key.includes(' ')) return `"${strippedKey}"`;
-  return strippedKey;
-}
-
-function toPrintedValue(val: EntityDefKeyValuePairValue, stripPrefix: string) {
-  if (typeof val === 'string')
-    return `"${
-      val.startsWith(stripPrefix) ? val.replace(stripPrefix, '') : val
-    }"`;
-  return val;
+  return new JsToEntityDefTransformer(
+    name,
+    keyValuePairs,
+    options
+  ).toEntityDef();
 }
